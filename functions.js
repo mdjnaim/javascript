3ee667015627aa1a2e1d9968ea80233828ea92e5
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-06 15:29:24 +0600

    callback

--- functions.js
+++ functions.js
@@ -6,8 +6,14 @@ function  factorial(n) {
 }
 console.log("Factorial of 5 is:", factorial(5));
 
+
 //2. Arrow Function
 const square = n => n*n;
 console.log(square(5));
 
+
 //3. Callback Function
+function runAfter4Sec(callback){
+    setTimeout(callback, 4000);
+}
+runAfter4Sec(() => console.log("This runs after 4 seconds"));
