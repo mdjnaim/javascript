b824549f585f5147a99c2d50322156a81c265f77
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-06 12:39:21 +0600

    switch case

--- fundamental.js
+++ fundamental.js
@@ -26,3 +26,25 @@ console.log("After swapping:", a, b);
 
 //4. Even or Odd
 console.log(a % 2 === 0 ? "Even" : "Odd");
+
+
+//5. Calculator using Switch Case
+function cal(a, b, operator){
+    switch(operator){
+        case '+':
+            return a+b;
+        case '-':
+            return a-b;
+        case '*':
+            return a*b;
+        case '/':
+            return b !== 0 ? a/b: "Can't divide by zero";
+        default:
+            return "Invalid operator";
+    }
+}
+console.log(cal(10, 5, '+'));
+console.log(cal(10, 50, '-'));
+console.log(cal(10, 5, '*'));
+console.log(cal(10, 5, '/'));
+console.log(cal(10, 0, '/'));
