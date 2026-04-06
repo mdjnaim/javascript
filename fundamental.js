8b51d0b9304a95159aa192b69978229bd67e6e53
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-06 12:21:40 +0600

    calculation

--- fundamental.js
+++ fundamental.js
@@ -4,3 +4,15 @@ let isStudent = true;
 console.log(typeof name);
 console.log(typeof age);
 console.log(typeof isStudent); 
+
+
+let a = 10;
+let b = 20;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let division = a / b;
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(division);
