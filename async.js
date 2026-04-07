cb6fcba7338854e8215457ce5f4599af8748d9bd
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-07 12:36:39 +0600

    Chained Promise

--- async.js
+++ async.js
@@ -12,3 +12,16 @@ function getUser(){
 });
 }
 getUser().then(user => console.log("User data:", user));
+
+
+//3. Chained Promises
+function getPosts(userId) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([{ id: 1, title: "Post 1"}])
+        }, 2000);
+    });
+}
+getUser()
+    .then(user => getPosts(user.id))
+    .then(posts => console.log("Posts for user:", posts));
