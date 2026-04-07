922cfc88ac2e044612a216bfc526ffaf00dab7fe
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-07 11:17:25 +0600

    closures

--- core.js
+++ core.js
@@ -3,3 +3,20 @@ sayHello();
 function sayHello() {
     console.log("Hello, World!");
 }
+
+
+//2. Closures
+function createCounter(){
+    let count = 0;
+    return {
+        increment: () => ++count,
+        decrement: () => --count,
+        getCount: () => count
+    };
+}
+const counter = createCounter();
+counter.increment();
+counter.increment();
+counter.increment();
+
+console.log("Current count:", counter.getCount());
