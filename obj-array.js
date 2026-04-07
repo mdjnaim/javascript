d248de4df10cf16f67752a2155fc79b7ddaa8abf
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-07 14:32:25 +0600

    odd even

--- obj-array.js
+++ obj-array.js
@@ -14,3 +14,14 @@ console.log(merged);
 //3. Map
 const doubled = [1, 2, 3].map(x => x * 2);
 console.log(doubled);
+
+const a = [1,2,3,4,5,6,7,8,9,10].map(n=>n*3);
+console.log(a);
+
+
+//4. Filter
+const oddNumbers = [1,2,3,4,5,6,7,8,9,10].filter(n => n % 2 !== 0);
+console.log("Odd numbers:", oddNumbers);
+
+const evenNumbers = [1,2,3,4,5,6,7,8,9,10].filter(n => n% 2===0);
+console.log("Even numbers:", evenNumbers);
