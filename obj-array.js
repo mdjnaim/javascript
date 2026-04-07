e02f4e0213b785cce20a72fe2e962278a20e3af7
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-07 14:23:51 +0600

    merge

--- obj-array.js
+++ obj-array.js
@@ -2,3 +2,10 @@
 const { name: userName, age: userAge } = {name: "Alice", age: 30};
 const [first, second] = [10, 20];
 console.log(userName, userAge,first, second);
+
+
+//2. Merge objects
+const obj1 = {a: 1, b: 2};
+const obj2 = {c: 3, d: 4};
+const merged = {...obj1, ...obj2};
+console.log(merged);
