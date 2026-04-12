516aa09fad5b15dae342764889b8e58570f4c12e
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-12 12:49:12 +0600

    1st

--- obj-array.js
+++ obj-array.js
@@ -1,7 +1,7 @@
 //1. Destructuring
 const { name: userName, age: userAge } = {name: "Alice", age: 30};
 const [first, second] = [10, 20];
-console.log(userName, userAge,first, second);
+console.log(userName, userAge, first, second);
 
 
 //2. Merge objects
---
+++ practice.js
@@ -0,0 +1,35 @@
+const a=5,b=6;
+const sum=a+b;
+console.log('Sum = '+sum)
+
+
+//////////
+const n = parseFloat(prompt('Enter a num = '));
+const m = parseFloat(prompt('Enter a num = '));
+
+const sum1=n+m;
+
+console.log(`The sum of ${n} and ${m} is ${sum1}`)
+
+////////////////
+
+
+function summ(x,y){
+   return (x+y);
+}
+const x = parseInt(prompt('Enter first value : '));
+const y = parseInt(prompt('Enter second value : '));
+console.log('Sum = '+summ(x,y));
+
+
+//////////
+const c=parseInt(prompt('1st value : '));
+const d=parseInt(prompt('2nd value : '));
+
+const summm=(c,d)=>{
+    return c+d
+}
+console.log('Sum = '+summm(c,d));
+
+
+
