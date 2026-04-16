a6c1a6486314e01f0aeef6d71cbdf06b0982f0a4
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-16 15:02:06 +0600

    cmt

--- practice.js
+++ practice.js
@@ -188,3 +188,14 @@ let num = prompt('Enter a number: ');
     }else{
         console.log('Input a correct number between 10 to 30 and 50 to 90.')
     }
+
+
+    /////////////////
+    let marks = [85,97,44,37,76,60];
+let sum=0
+for(let val of marks){
+    sum = sum + val;
+    console.log("Total mark is : ", sum);
+}
+let avg = sum/marks.length;
+console.log("Average mark is : "+avg);
