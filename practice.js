bacbaf10b4e244ed86bc09a73095b79009ab5e7d
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-19 12:28:48 +0600

    sqr

--- practice.js
+++ practice.js
@@ -235,3 +235,12 @@ const vowel = (str) => {
     console.log('Vowel count: ',count);
 }
 vowel('asdfghjklzxcvbnmqwertyuiop');
+
+///////////////
+function squareNum(){
+    let arr = [1,2,3,4,5,6,7,8,9,10];
+    for(let i=0; i<arr.length; i++){
+        console.log(arr[i]*arr[i]);
+    }
+}
+squareNum();
