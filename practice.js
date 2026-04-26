8c084f482034928b67e2fcbb95bf2947e3c6a232
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-26 15:49:20 +0600

    prblm

--- practice.js
+++ practice.js
@@ -625,3 +625,30 @@ function main() {
     
     console.log(re.test(s));
 }
+
+///////////
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+   
+    const firstInteger = 4;
+    
+    const firstDecimal = 4.0;
+    
+    const firstString = 'HackerRank ';
+    
+    console.log(firstInteger + parseInt(secondInteger));
+    
+    console.log(firstDecimal + parseFloat(secondDecimal));
+    
+    console.log(firstString + secondString);
+
+}   
+
+
+function main() {
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+    
+    performOperation(secondInteger, secondDecimal, secondString);
+}
