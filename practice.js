2d1fe87be67f138b41e8784024b7160c06733786
mdjnaim <21-45039-2@student.aiub.edu>
2026-04-27 12:02:00 +0600

    'factorial

--- practice.js
+++ practice.js
@@ -673,3 +673,15 @@ function main() {
     
     reverseString(s);
 }
+
+////////////////
+
+function factorial(n){
+    let result = 1;
+   for(let i=n; i>0; i--){
+     result *= i;
+   }
+   console.log(`Factorial of ${n} is : ${result}`);
+   return result;
+}
+factorial(5);
